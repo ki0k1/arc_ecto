4e2d18cba0db590951c1429326b178a852a859b9
ki0k1 <ki0k1@users.noreply.github.com>
2017-03-19 21:12:22 +0300

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule Arc.Ecto.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [
-      {:arc,  "~> 0.7.0"},
+      {:arc,  git: "https://github.com/ki0k1/arc"},
       {:ecto, "~> 2.0"},
       {:mock, "~> 0.1.1", only: :test},
       {:ex_doc, ">= 0.0.0", only: :dev}
